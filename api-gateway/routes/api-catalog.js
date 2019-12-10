8afc8517b2a2cdd30fa3c33a2ebc55dbf7fbe050
dearwendy <dearwendy714@gmail.com>
2019-12-09 19:11:32 -0800

    update week-8

--- api-gateway/routes/api-catalog.js
+++ api-gateway/routes/api-catalog.js
@@ -10,13 +10,14 @@
 /**
  * API Routes
  */
+var checkToken = require("../ check-token");
 var express = require('express');
 var router = express.Router();
 var auth_controller = require('../controllers/authController');
 // POST request for registering a user
 router.post('/auth/register', auth_controller.user_register);
 // GET request for verifying user tokens
-router.get('/auth/token', auth_controller.user_token);
+router.get('/auth/token', checkToken, auth_controller.user_token);
 // POST request for logging in
 router.post('/auth/login', auth_controller.user_login);
 // GET request for login out user
